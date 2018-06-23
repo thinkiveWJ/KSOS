7d59da4e23d5113c14f26f846416b44c955d1287
thinkiveWJ <wangjunzmd@163.com>
2018-06-23 22:05:10 +0800

    用户新增

--- src/components/userManager/userManagerAdd.vue
+++ src/components/userManager/userManagerAdd.vue
@@ -1,23 +1,6 @@
 <template>
 	<div id="userManagerAdd">
 		<dl>
-			<!-- <div class="clearfix">
-				<dt>用户名：</dt>
-				<dd><el-input v-model.trim="userName" placeholder="请输入用户名"></el-input></dd>
-			</div>
-			<div class="clearfix">
-				<dt>身份：</dt>
-				<dd>
-					<el-select v-model="identityVal" placeholder="请选择" style="width:100%;">
-					    <el-option
-					      v-for="item in identity"
-					      :key="item.value"
-					      :label="item.text"
-					      :value="item.value">
-					    </el-option>
-					</el-select>
-				</dd>
-			</div> -->
 			<div class="clearfix">
 				<dt>手机号：</dt>
 				<dd><el-input v-model.trim="phone" placeholder="请输入手机号"></el-input></dd>
@@ -40,19 +23,6 @@
 	export default {
 		data () {
 			return {
-				// userName: '',
-//				身份信息
-				// identityVal: '',
-				// identity: [
-				// 	{
-				// 		text: "委托人", 
-				// 		value: 1
-				// 	},
-				// 	{
-				// 		text: "捎件人",
-				// 		value: 2
-				// 	}
-				// ],
 				phone: '',
 				password: '',
 				password2: ''
@@ -60,16 +30,6 @@
 		},
 		methods: {
 			addUser () {
-				// if (isEmpty(this.userName)) {
-				// 	return this.$notify.error({
-			    //       message: '请输入用户名'
-			    //     });
-				// }
-				// if (isEmpty(this.identityVal)) {
-				// 	return this.$notify.error({
-			    //       message: '请选择身份'
-			    //     });
-				// }
 				if (isEmpty(this.phone)) {
 					return this.$notify.error({
 			          message: '请输入手机号码'
@@ -109,12 +69,6 @@
 			          message: '新增用户成功',
 			          type: 'success'
 					});
-					
-//			        this.userName = '';
-//					this.identityVal = '';
-//					this.phone = '';
-//					this.password = '';
-//					this.password2 = '';
 					console.log(this);
 					this.$router.push({
 						path: '/userManager/query'
