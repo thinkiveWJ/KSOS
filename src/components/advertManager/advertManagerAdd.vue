f429a0b942c52b28e5fd7685ea94109a3b4f23c4
wangjunzmd@163.com <wangjunzmd@163.com>
2018-06-27 11:30:14 +0800

    图片上传

--- src/components/advertManager/advertManagerAdd.vue
+++ src/components/advertManager/advertManagerAdd.vue
@@ -1,15 +1,16 @@
 <template>
 	<div id="advertManagerAdd">
-		<form id="advertManagerAddForm" action="/FlightDeliveryServer/FlightDeliveryServer/admanagement/createad" method="post" enctype="multipart/form-data">
+		<!-- <iframe style="display:none" name="iframe"></iframe>
+		<form id="advertManagerAddForm" target="iframe" action="/FlightDeliveryServer/FlightDeliveryServer/admanagement/createad" method="post" enctype="multipart/form-data"> -->
 		<dl>
 			<div class="clearfix">
 				<dt>版本名称：</dt>
-				<dd><el-input v-model.trim="name" placeholder="请输入版位名称"></el-input></dd>
+				<dd><el-input name="name" v-model.trim="imgUploadData.name" placeholder="请输入版位名称"></el-input></dd>
 			</div>
 			<div class="clearfix">
 				<dt>版位类型：</dt>
 				<dd>
-					<el-select v-model="type" placeholder="请选择">
+					<el-select v-model="imgUploadData.type" name="type" placeholder="请选择">
 					    <el-option
 					      v-for="item in typeList"
 					      :key="item.value"
@@ -21,30 +22,41 @@
 			</div>
 			<div class="clearfix">
 				<dt>版位尺寸（宽：px）：</dt>
-				<dd><el-input v-model.trim="width" type="number" placeholder="请输入版位尺寸的宽"></el-input></dd>
+				<dd><el-input v-model.trim="imgUploadData.width" name="width" type="number" placeholder="请输入版位尺寸的宽"></el-input></dd>
 			</div>
 			<div class="clearfix">
 				<dt>版位尺寸（高：px）：</dt>
-				<dd><el-input v-model.trim="height" type="number" placeholder="请输入版位尺寸的高"></el-input></dd>
+				<dd><el-input v-model.trim="imgUploadData.height" name="height" type="number" placeholder="请输入版位尺寸的高"></el-input></dd>
 			</div>
 			<div class="clearfix">
 				<dt>版位描述：</dt>
-				<dd><el-input v-model.trim="desc"  placeholder="请输入版位描述"></el-input></dd>
+				<dd><el-input v-model.trim="imgUploadData.desc" name="desc"  placeholder="请输入版位描述"></el-input></dd>
 			</div>
 			<div class="clearfix">
-				<dt class="pull-left">文件上传：</dt>
+				<dt class="pull-left">图片上传：</dt>
 				<dd class="pull-left">
+					<!-- <input type="file" name="image"/> -->
 					<el-upload
+					  ref="upload"
 					  class="upload-btn"
-					  action=""
+					  :auto-upload="false"
+					  action = "/FlightDeliveryServer/FlightDeliveryServer/admanagement/createad"
+					  :limit='1' 
+					  name = "image"
+					  :data = "imgUploadData"
+					  :file-list="imgList"
+					  :on-error="uploadError"
+					  :on-success="uploadSuccess"
+					  :on-change="onChange"
 					  >
-					  <el-button size="small" type="primary">点击上传</el-button>
+					  <el-button size="small" type="primary">上传图片</el-button>
+						<div class="el-upload__tip">只能上传jpg/png文件</div>
 					</el-upload>
 				</dd>
 			</div>
 			<a class="btn-save" @click="addCoupon">保存</a>
 		</dl>
-		</form>
+		<!-- </form> -->
 	</div>
 </template>
 
@@ -53,73 +65,78 @@
 	export default {
 		data () {
 			return {
-				name: '',
-				type: '',
-				width: '',
-				height: '',
-				desc: '',
+				imgList: [],
+				imgUploadData: {
+					name: "",
+					type: "",
+					width: "",
+					height: "",
+					desc: ""
+				},
 				typeList: [
 					{
 						text: '图片轮换广播',
-						value: 1
+						value: 3
 					}
-				],
+				]
 			}
 		},
 		methods: {
+			onChange (event, file, fileList) {
+				let filename = event['name']
+				let filesize = event['size']
+				let fileType = filename.split(".")[1]
+				if(fileType !== 'jpg' && fileType !== 'png'){
+					this.imgList = [];
+					this.$notify.error({
+			          message: '图片格式不正确'
+					});
+					this.$refs.upload.clearFiles();
+					return false;
+				}
+			},
+			uploadError (err, file, fileList) {
+				this.$refs.upload.clearFiles();
+			},
+			uploadSuccess (response, file, fileList) {
+				if(response['error_code'] !== 0){
+					this.$refs.upload.clearFiles();
+					return this.$notify.error({
+						message: response['error_msg']
+					});
+				}
+			},
+			// 点击保存
 			addCoupon () {
-				if (isEmpty(this.name)) {
+				if (isEmpty(this.imgUploadData.name)) {
 					return this.$notify.error({
 			          message: '请输入版位名称'
 			        });
 				}
-				if (isEmpty(this.type)) {
+				if (isEmpty(this.imgUploadData.type)) {
 					return this.$notify.error({
 			          message: '请选择版位类型'
 			        });
 				}
-				if (isEmpty(this.width)) {
+				if (isEmpty(this.imgUploadData.width)) {
 					return this.$notify.error({
 			          message: '请输入版位宽度'
 			        });
 				}
-				if (isEmpty(this.height)) {
+				if (isEmpty(this.imgUploadData.height)) {
 					return this.$notify.error({
 			          message: '请输入版位高度'
 			        });
 				}
-				if (isEmpty(this.desc)) {
+				if (isEmpty(this.imgUploadData.desc)) {
 					return this.$notify.error({
 			          message: '请输入版位描述信息'
 			        });
 				}
-				$("#advertManagerAddForm").submit();
-// 				this.$ajax(this, {
-// 					url: '/createAd',
-// 					data: {
-// 						name: this.name,
-// 						type: this.type,
-// 						width: this.width,
-// 						height: this.height,
-// 						desc: this.desc
-// 					}
-// 				}, 
-// 				(result) => {
-// 					this.$notify({
-// 			          message: '新增广告版位成功',
-// 			          type: 'success'
-// 			        });
-// //			        this.name = '';
-// //					this.type = '';
-// //					this.width = '';
-// //					this.height = '';
-// //					this.desc = '';
-// 					this.$router.push({
-// 						path: '/advertManager/query'
-// 					});
-// 				});
+				this.$refs.upload.submit();
 			}
 		}
+		
 	}
 </script>
 
